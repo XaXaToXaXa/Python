a228fb392617b155f01ff81f542d7161867241c9
XaXaToXaXa <XaXaToXaXa@gmail.com>
2023-02-16 19:52:20 +0300

    Add elif

--- Lectutre_One.py
+++ Lectutre_One.py
@@ -14,3 +14,26 @@
 # print ("Введи что нибудь еще: ")
 # b = input ()
 # print (f"Сложениее первого и второго = { int(a) + int(b)}")
+
+# a = 5.65464
+# b = 4.543
+# print (round(a*b, 3))
+
+# iter = 2
+# iter += 3 # iter = iter + 3
+# iter -= 4 # iter = iter - 4
+# iter *= 5 # iter = iter * 5
+# iter /= 5 # iter = iter / 5
+# iter //= 5 # iter = iter // 5
+# iter %= 5 # iter = iter % 5
+# iter **= 5 # iter = iter ** 5
+
+username = input('Введите имя: ')
+if username == 'Маша':
+    print('Ура, это же МАША!')
+elif username == 'Марина':
+    print('Я так ждала Вас, Марина!')
+elif username == 'Ильнар':
+    print('Ильнар - топ)')
+else:
+    print('Привет, ', username)
