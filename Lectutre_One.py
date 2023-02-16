25ff9e0e8d6f806115cf1ea3829bfd39252d4ce6
XaXaToXaXa <XaXaToXaXa@gmail.com>
2023-02-16 20:16:47 +0300

    2

--- Lectutre_One.py
+++ Lectutre_One.py
@@ -28,12 +28,56 @@
 # iter %= 5 # iter = iter % 5
 # iter **= 5 # iter = iter ** 5
 
-username = input('Введите имя: ')
-if username == 'Маша':
-    print('Ура, это же МАША!')
-elif username == 'Марина':
-    print('Я так ждала Вас, Марина!')
-elif username == 'Ильнар':
-    print('Ильнар - топ)')
-else:
-    print('Привет, ', username)
+# username = input('Введите имя: ')
+# if username == 'Маша':
+#     print('Ура, это же МАША!')
+# elif username == 'Марина':
+#     print('Я так ждала Вас, Марина!')
+# elif username == 'Ильнар':
+#     print('Ильнар - топ)')
+# else:
+#     print('Привет, ', username)
+
+# n = 423
+# summa = 0
+# while summa > 0:
+#     x = n % 10
+#     summa = summa + x
+#     n = n // 10
+# print(summa) # 9
+
+# i = 0
+# while i < 5:
+#     if i == 3:
+#         # break
+#         i = i + 1
+# else:
+#     print('Пожалуй')
+#     print('надоело')
+# print(i)
+
+# n = 423
+# summa = 0
+# while summa > 0:
+#     x = n % 10
+#     summa = summa + x
+#     n = n // 10
+# else:
+#     print('Пожалуй')
+#     print('хватит )')
+# print(summa)
+# # Пожалуй
+# # хватит )
+# # 9
+
+# n = int(input())
+# flag = True
+# i = 2
+# while flag:
+#     if n % i == 0: # если остаток при делении числа n на i равен 0
+#         flag = False
+#         print(i)
+#     elif i > n // 2: # делить числа не может превышать введенное число, деленное на 2
+#         print(n)
+#         flag = False
+#     i += 1
