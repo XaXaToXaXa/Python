7c23a4174f5dbcc2f5f0572fd354fd279eb30f6b
XaXaToXaXa <XaXaToXaXa@gmail.com>
2023-02-16 19:51:52 +0300

    Add if

---
+++ Lectutre_One.py
@@ -0,0 +1,16 @@
+# print (5)
+# print (5)
+# print (5)
+
+# a = 5
+# b = 5.89
+# c = 'Hello'
+
+# print (f"{a} и {b} и еще {c}")
+# print ("{} -{} - {}".format(a,b,c))
+
+# a = input ("Введи что нибудь: ")
+# print (f" Ты ввел {a}")
+# print ("Введи что нибудь еще: ")
+# b = input ()
+# print (f"Сложениее первого и второго = { int(a) + int(b)}")
