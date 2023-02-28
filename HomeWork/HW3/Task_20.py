d9107fe27fd524848073d23fe87d89dcf7467e6f
xaxatoxaxa <xaxatoxaxa@gmail.com>
2023-02-28 16:42:50 +0300

    Finish Task_20

--- HomeWork/HW3/Task_20.py
+++ HomeWork/HW3/Task_20.py
@@ -22,3 +22,30 @@
 #
 # ноутбук
 #     12
+
+
+# English letters
+"""
+import re
+input_word = input("Введите слово: ")
+diction = {'[AaEeIiOoUuLlNnSsTtRr]': '1', '[DdGg]': '2', '[BbCcMmPp]': '3',
+           '[FfHhVvWwYy]': '4', 'Kk': '5', '[JjXx]': '8', '[QqZz]': '10'}
+for key in diction:
+    input_word = re.sub(key, diction[key], input_word)
+print(sum(map(int, input_word)))
+"""
+
+# Русские буквы
+diction = {'АаВвЕеИиНнОоРрСсТт': 1, 'ДдКкЛлМмПпУу': 2,
+           'БбГгЁёЬьЯя': 3, 'ЙйЫы': 4, 'ЖжЗзХхЦцЧч': 5, 'ШшЭэЮю': 8, 'ФфЩщЪъ': 10}
+
+
+def scrabble(letter):
+    for key in diction:
+        if letter in key:
+            return diction.get(key)
+
+
+input_word = input("Введите слово: ")
+
+print(f"Вы набрали {sum(map(scrabble, input_word))} очков")
