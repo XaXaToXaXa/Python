d53afdd5eba8db399708217e30a99c951242bda8
xaxatoxaxa <xaxatoxaxa@gmail.com>
2023-02-28 13:32:42 +0300

    Complete Task_16

--- HomeWork/HW3/Task_16.py
+++ HomeWork/HW3/Task_16.py
@@ -8,3 +8,26 @@
 #     1 2 3 4 5
 #     3
 #     -> 1
+#Вариант с последовательным массивом.
+"""
+list_size = int(input("Введите размер массива: "))
+find_num = int(input("Введите искомое значение: "))
+num_list = []
+count = 0
+for i in range(list_size):
+    num_list.append(i+1)
+    if find_num == num_list[i]:
+        count = count+1
+print(f"В массиве :{num_list} число: {find_num} встречается: {count} раз.")
+"""
+#Вариант с рандомным массивом.
+import random
+list_size = int(input("Введите размер массива: "))
+find_num = int(input("Введите искомое значение: "))
+num_list = [1,]
+count = 0
+for i in range(list_size):
+    num_list.append(random.randint(1, 20))
+    if find_num == num_list[i]:
+        count = count+1
+print(f"В массиве :{num_list} число: {find_num} встречается: {count} раз.")
