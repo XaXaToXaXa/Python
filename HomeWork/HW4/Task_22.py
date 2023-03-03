aebc1dc21a844ba192c8b59f14ee85b899e71e91
xaxatoxaxa <xaxatoxaxa@gmail.com>
2023-03-03 15:09:08 +0300

    Add task 22

--- HomeWork/HW4/Task_22.py
+++ HomeWork/HW4/Task_22.py
@@ -8,3 +8,28 @@
 # собирает ягоды с этого куста и с двух соседних с ним.
 # Напишите программу для нахождения максимального числа ягод, 
 # которое может собрать за один заход собирающий модуль, находясь перед некоторым кустом заданной во входном файле грядки.
+
+import random
+
+
+def fill_list(size):
+    new_list = []
+    for i in range(size):
+        new_list.append(random.randint(1, 20))
+    return new_list
+
+
+
+size = int(input("Введите количество кустов: "))
+bush_list = fill_list(size)
+print(f"На кустах растет: {bush_list} ягод.")
+if size <= 3:
+    print(sum(bush_list))
+else:
+    result = 0
+    for i in range(size):
+        summ = bush_list[i-1] + bush_list[i] + bush_list[(i+1)%size]
+        if summ > result: 
+            result = summ
+            temp = (i-1, i, (i+1)%size)
+    print(f"Максимально модуль сможет собрать: {result}")
