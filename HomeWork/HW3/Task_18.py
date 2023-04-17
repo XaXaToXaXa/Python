d47c3cca09f5376b1690105f87acaf261087a597
XaXaToXaXa <XaXaToXaXa@gmail.com>
2023-04-17 20:43:08 +0300

    Fix 18 Task

--- HomeWork/HW3/Task_18.py
+++ HomeWork/HW3/Task_18.py
@@ -10,27 +10,29 @@
 #     -> 5
 
 # Вариант с последовательным массивом.
-""""
-def fill_list(size):
+
+def input_fill_list(size):
     new_list = []
     for i in range(size):
         new_list.append(int(input("Введите натуральное число: ")))
     return new_list
-"""
+
 # Рандом
 import random
 
-def fill_list(size):
+def random_fill_list(size):
     new_list = []
     for i in range(size):
-        new_list.append(random.randint(1, 20))
+        new_list.append(random.randint(1, 10))
     return new_list
 
 
 list_size = int(input("Введите размер массива: "))
 find_num = int(input("Введите искомое значение: "))
-
-my_list = fill_list(list_size)
+"""
+# Вариант через цикл
+# my_list = input_fill_list(list_size)
+my_list = random_fill_list(list_size)
 closest_num = my_list[0]
 for i in my_list:
     if abs(i-find_num) <= abs(closest_num-find_num):
@@ -38,3 +40,7 @@ for i in my_list:
    
 
 print(f"В массиве :{my_list} ближайшее число к: {find_num} является: {closest_num}.")
+"""
+# Вариант через min
+my_list = random_fill_list(list_size)
+print(f"В массиве :{my_list} ближайшее число к: {find_num} является: {min(my_list, key=lambda x: abs(x-find_num))}.")
